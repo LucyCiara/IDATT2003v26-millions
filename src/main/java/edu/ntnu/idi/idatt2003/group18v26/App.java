f840e59dc06214687355ca8acecf7f03de41c48b
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-02-06 13:43:59 +0100

    Init[]: Initialize Maven

---
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/App.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idi.idatt2003.group18v26;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
