fa364033a506cc10634f835309d8c2a7a4738947
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-02-26 15:01:45 +0100

    feat[Transaction]: create Transaction superclass

---
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package edu.ntnu.idi.idatt2003.group18v26.transaction;
+
+import edu.ntnu.idi.idatt2003.group18v26.model.Player;
+import edu.ntnu.idi.idatt2003.group18v26.model.property.Share;
+
+public class Transaction {
+  protected Share share;
+  protected int week;
+  protected TransactionCalculator calculator;
+  protected boolean committed;
+
+  protected Transaction(Share share, int week, TransactionCalculator calculator) {
+    this.share = share;
+    this.week = week;
+    this.calculator = calculator;
+  }
+
+  public Share getShare() {
+    return this.share;
+  }
+
+  public int getWeek() {
+    return this.week;
+  }
+
+  public TransactionCalculator getCalculator() {
+    return this.TransactionCalculator;
+  }
+
+  public boolean isCommited() {
+    return this.committed;
+  }
+
+  public void commit(Player player) {
+  }
+
+}
