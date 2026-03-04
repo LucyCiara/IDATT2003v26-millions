344ee11a53237cd9c3c4eefbf1d76fd66eb3443e
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-04 19:18:51 +0100

    test[Player]: add tests for Player class

--- src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/transaction/Sale.java
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/transaction/Sale.java
@@ -2,7 +2,6 @@ package edu.ntnu.idi.idatt2003.group18v26.model.transaction;
 
 import edu.ntnu.idi.idatt2003.group18v26.model.Player;
 import edu.ntnu.idi.idatt2003.group18v26.model.property.Share;
-import edu.ntnu.idi.idatt2003.group18v26.transaction.Transaction;
 
 public class Sale extends Transaction {
   public Sale(Share share, int week) {
