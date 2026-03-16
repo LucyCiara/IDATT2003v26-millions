9f2c7266dad5af77d645ef393b4f8c4fff902abd
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-16 16:10:19 +0100

    update[exchange]: add parameter validation with a util class

--- src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/property/Exchange.java
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/property/Exchange.java
@@ -10,6 +10,7 @@ import edu.ntnu.idi.idatt2003.group18v26.model.Player;
 import edu.ntnu.idi.idatt2003.group18v26.model.transaction.Purchase;
 import edu.ntnu.idi.idatt2003.group18v26.model.transaction.Sale;
 import edu.ntnu.idi.idatt2003.group18v26.model.transaction.Transaction;
+import edu.ntnu.idi.idatt2003.group18v26.util.ParameterValidator;
 
 public class Exchange {
   private String name;
@@ -18,6 +19,8 @@ public class Exchange {
   private Random random;
 
   public Exchange(String name, List<Stock> stocks) {
+    ParameterValidator.stringChecker(name, "name");
+    ParameterValidator.objectChecker(stocks, "stocks");
     this.name = name;
     stocks.forEach(stock -> this.stockMap.put(stock.getSymbol(), stock));
   }
@@ -39,6 +42,7 @@ public class Exchange {
   }
 
   public List<Stock> findStocks(String searchTerm) {
+    ParameterValidator.stringChecker(searchTerm, "searchTerm");
     List<Stock> resultStocks = new ArrayList<Stock>();
     for (Stock stock : this.stockMap.values()) {
       if (stock.getSymbol().contains(searchTerm) || stock.getCompany().contains(searchTerm)) {
@@ -49,17 +53,29 @@ public class Exchange {
   }
 
   public Transaction buy(String symbol, BigDecimal quantity, Player player) {
+    ParameterValidator.stringChecker(symbol, "symbol");
+    ParameterValidator.bigDecimalChecker(quantity, "quantity");
+    ParameterValidator.objectChecker(player, "player");
     Transaction purchase = new Purchase(
         new Share(this.stockMap.get(symbol), quantity, this.stockMap.get(symbol).getSalesPrice()), this.week);
     purchase.commit(player);
     return purchase;
   }
 
-  public Transaction sell(String symbol, Player player) {
+  public Transaction sell(Share share, Player player) {
+    ParameterValidator.objectChecker(share, "share");
+    ParameterValidator.objectChecker(player, "player");
     Transaction sale = new Sale(
-        new Share(this.stockMap.get(symbol), this.stockMap.get(symbol).getSalesPrice()),
+        share,
         this.week);
     sale.commit(player);
     return sale;
+  }
+
+  public void advance() {
+    for (Stock stock : this.stockMap.values()) {
+      stock.addNewSalesPrice(stock.getSalesPrice().multiply(new BigDecimal(this.random.nextDouble() + 0.5)));
+    }
+    this.week++;
   }
 }
