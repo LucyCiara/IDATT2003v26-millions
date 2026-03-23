6c7f4845f5942c5ca78d212e4b6e6e4788d9b456
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-23 14:10:10 +0100

    test[CsvStockWriter]: add test for invalid path

--- src/test/java/edu/ntnu/idi/idatt2003/group18v26/model/filehandling/CsvStockWriterTest.java
+++ src/test/java/edu/ntnu/idi/idatt2003/group18v26/model/filehandling/CsvStockWriterTest.java
@@ -10,24 +10,27 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CsvStockWriterTest {
   private CsvStockWriter writer;
+  private List<Stock> stocks;
 
   @BeforeEach
   void setUp() {
     writer = new CsvStockWriter();
+    this.stocks = List.of(
+      new Stock("AAPL", "Apple Inc.", new BigDecimal("150.00")),
+      new Stock("GOOGL", "Alphabet Inc.", new BigDecimal("2800.00"))
+    );
   }
 
   @Test
   void testWriteStocksSucceeds() throws IOException {
     Path tempFile = Files.createTempFile("Valid", ".csv");
     try {
-        List<Stock> stocks = List.of(
-            new Stock("AAPL", "Apple Inc.", new BigDecimal("150.00")),
-            new Stock("GOOGL", "Alphabet Inc.", new BigDecimal("2800.00"))
-        );
-        writer.writeStocks(stocks, tempFile);
+
+        writer.writeStocks(this.stocks, tempFile);
 
         List<String> lines = Files.readAllLines(tempFile);
         assertEquals(4, lines.size());
@@ -37,6 +40,26 @@ public class CsvStockWriterTest {
         assertEquals("GOOGL,Alphabet Inc.,2800.00", lines.get(3));
     } finally {
         Files.deleteIfExists(tempFile);
+    }
+  }
+
+  @Test
+  void nonExistentPathThrowsException() {
+    Path invalidPath = Path.of("src/test/resources/nonexistentDirectory/nonexistent.csv");
+    IOException exception = assertThrows(
+        IOException.class, () -> writer.writeStocks(this.stocks, invalidPath)
+    );
+    String os = System.getProperty("os.name");
+    if (os.contains("Windows")) {
+      assertEquals(
+          "src\\test\\resources\\nonexistentDirectory\\nonexistent.csv",
+          exception.getMessage()
+      );
+    } else {
+      assertEquals(
+          "src/test/resources/nonexistentDirectory/nonexistent.csv",
+          exception.getMessage()
+      );
     }
   }
 }
