cf377b2284190f32c32dab5e73a671fc6406ff37
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-25 16:37:57 +0100

    test[player]: add test for getStatus

--- src/test/java/edu/ntnu/idi/idatt2003/group18v26/model/PlayerTest.java
+++ src/test/java/edu/ntnu/idi/idatt2003/group18v26/model/PlayerTest.java
@@ -3,8 +3,10 @@ package edu.ntnu.idi.idatt2003.group18v26.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import edu.ntnu.idi.idatt2003.group18v26.model.property.Portfolio;
 import edu.ntnu.idi.idatt2003.group18v26.model.property.Share;
 import edu.ntnu.idi.idatt2003.group18v26.model.property.Stock;
+import edu.ntnu.idi.idatt2003.group18v26.model.transaction.Purchase;
 import edu.ntnu.idi.idatt2003.group18v26.model.transaction.SaleCalculator;
 
 /**
@@ -213,5 +216,122 @@ public class PlayerTest {
         = new Share(new Stock("A", "B", BigDecimal.TEN), BigDecimal.TWO, BigDecimal.TEN);
     testPlayer.getPortfolio().addShare(testShare);
     expectedWorth = expectedWorth.add(new SaleCalculator(testShare).calculateTotal());
+  }
+
+  @Test
+  void getStatusReturnsExpectedStatus() {
+    Player testPlayer = new Player(this.testName, this.testStartingMoney);
+    String expectedStatus = "Novice";
+    assertEquals(0, testPlayer.getTransactionArchive().countDistinctWeeks());
+    assertEquals(0, testPlayer.getNetWorth().compareTo(testStartingMoney));
+    assertEquals(expectedStatus, testPlayer.getStatus());
+
+    BigDecimal investorProfit = this.testStartingMoney.multiply(new BigDecimal(0.2));
+    testPlayer.addMoney(investorProfit);
+    assertEquals(expectedStatus, expectedStatus);
+    testPlayer.withdrawMoney(investorProfit);
+
+    for (int i = 0; i < 10; i++) {
+      new Purchase(
+        new Share(
+          new Stock("A", "B", BigDecimal.ONE),
+          BigDecimal.ONE, BigDecimal.ONE
+        ),
+        i
+      ).commit(testPlayer);
+      BigDecimal cost
+          = testPlayer.getTransactionArchive().getPurchases(i).getFirst().getCalculator()
+              .calculateTotal();
+      testPlayer.addMoney(cost);
+    }
+
+    BigDecimal stockValue = testPlayer.getPortfolio().getNetWorth();
+    if (stockValue.compareTo(BigDecimal.ZERO) > 0) {
+      testPlayer.withdrawMoney(stockValue);
+    } else if (stockValue.compareTo(BigDecimal.ZERO) < 0) {
+      testPlayer.addMoney(stockValue.multiply(new BigDecimal(-1)));
+    }
+
+    assertEquals(10, testPlayer.getTransactionArchive().countDistinctWeeks());
+    assertEquals(-1,
+      testPlayer.getNetWorth().divide(
+        testStartingMoney,
+        1,
+        RoundingMode.FLOOR
+      ).compareTo(new BigDecimal(1.2))
+    );
+    assertEquals(expectedStatus, testPlayer.getStatus());
+
+    expectedStatus = "Investor";
+    testPlayer.addMoney(investorProfit);
+    assertEquals(10, testPlayer.getTransactionArchive().countDistinctWeeks());
+    assertTrue(
+      testPlayer.getNetWorth().divide(
+        testStartingMoney,
+        1,
+        RoundingMode.FLOOR
+      ).compareTo(new BigDecimal(1.2)) >= -1
+    );
+    assertEquals(expectedStatus, testPlayer.getStatus());
+
+    BigDecimal speculatorProfit = this.testStartingMoney;
+    testPlayer.withdrawMoney(investorProfit);
+    testPlayer.addMoney(speculatorProfit);
+    assertEquals(10, testPlayer.getTransactionArchive().countDistinctWeeks());
+    assertTrue(
+      testPlayer.getNetWorth().divide(
+        testStartingMoney,
+        1,
+        RoundingMode.FLOOR
+      ).compareTo(new BigDecimal(2.0)) >= -1
+    );
+    assertEquals(expectedStatus, testPlayer.getStatus());
+    testPlayer.withdrawMoney(speculatorProfit);
+    testPlayer.addMoney(investorProfit);
+
+    for (int i = 10; i < 20; i++) {
+      new Purchase(
+        new Share(
+          new Stock("A", "B", BigDecimal.ONE),
+          BigDecimal.ONE, BigDecimal.ONE
+        ),
+        i
+      ).commit(testPlayer);
+      BigDecimal cost
+          = testPlayer.getTransactionArchive().getPurchases(i).getFirst().getCalculator()
+              .calculateTotal();
+      testPlayer.addMoney(cost);
+    }
+
+    stockValue = testPlayer.getPortfolio().getNetWorth().subtract(stockValue);
+    if (stockValue.compareTo(BigDecimal.ZERO) > 0) {
+      testPlayer.withdrawMoney(stockValue);
+    } else if (stockValue.compareTo(BigDecimal.ZERO) < 0) {
+      testPlayer.addMoney(stockValue.multiply(new BigDecimal(-1)));
+    }
+
+    assertEquals(20, testPlayer.getTransactionArchive().countDistinctWeeks());
+    assertEquals(-1,
+      testPlayer.getNetWorth().divide(
+        testStartingMoney,
+        1,
+        RoundingMode.FLOOR
+      ).compareTo(new BigDecimal(2.0))
+    );
+    assertEquals(expectedStatus, testPlayer.getStatus());
+
+    expectedStatus = "Speculator";
+    testPlayer.withdrawMoney(investorProfit);
+    testPlayer.addMoney(speculatorProfit);
+    assertEquals(20, testPlayer.getTransactionArchive().countDistinctWeeks());
+    assertTrue(
+      testPlayer.getNetWorth().divide(
+        testStartingMoney,
+        1,
+        RoundingMode.FLOOR
+      ).compareTo(new BigDecimal(2.0)) >= -1
+    );
+    assertEquals(expectedStatus, testPlayer.getStatus());
+
   }
 }
