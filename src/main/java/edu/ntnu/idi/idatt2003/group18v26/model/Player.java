3e2ac3b373cf088e8b919d6df1756e1af4f53368
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-25 16:37:13 +0100

    fix[player]: fix precision issues in getStatus

--- src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/Player.java
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/Player.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idi.idatt2003.group18v26.model;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import edu.ntnu.idi.idatt2003.group18v26.model.property.Portfolio;
 import edu.ntnu.idi.idatt2003.group18v26.model.transaction.TransactionArchive;
@@ -106,10 +107,12 @@ public class Player {
 
   public String getStatus() {
     int weeksOfTrade = this.getTransactionArchive().countDistinctWeeks();
-    BigDecimal profit = this.getNetWorth().multiply(this.startingMoney).subtract(BigDecimal.ONE);
+    BigDecimal profit = this.getNetWorth().divide(this.startingMoney, 1, RoundingMode.FLOOR);
+    profit = profit.subtract(BigDecimal.ONE);
     if (weeksOfTrade >= 20 && profit.compareTo(BigDecimal.ONE) >= 0) {
       return "Speculator";
-    } else if (weeksOfTrade >= 10 && profit.compareTo(new BigDecimal(0.2)) >= 0) {
+    } else if (weeksOfTrade >= 10
+        && profit.compareTo(new BigDecimal(0.2).setScale(1, RoundingMode.FLOOR)) >= 0) {
       return "Investor";
     } else {
       return "Novice";
