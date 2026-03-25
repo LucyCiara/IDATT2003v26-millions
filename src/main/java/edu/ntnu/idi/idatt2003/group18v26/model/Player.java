7e1500e3dacdcfbaf59c2356636095346a91bd71
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-25 15:03:32 +0100

    feat[player]: add getStatus method

--- src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/Player.java
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/Player.java
@@ -103,4 +103,16 @@ public class Player {
   public BigDecimal getNetWorth() {
     return this.money.add(this.portfolio.getNetWorth());
   }
+
+  public String getStatus() {
+    int weeksOfTrade = this.getTransactionArchive().countDistinctWeeks();
+    BigDecimal profit = this.getNetWorth().multiply(this.startingMoney).subtract(BigDecimal.ONE);
+    if (weeksOfTrade >= 20 && profit.compareTo(BigDecimal.ONE) >= 0) {
+      return "Speculator";
+    } else if (weeksOfTrade >= 10 && profit.compareTo(new BigDecimal(0.2)) >= 0) {
+      return "Investor";
+    } else {
+      return "Novice";
+    }
+  }
 }
