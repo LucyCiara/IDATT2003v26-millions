6f8216328920d85abd98030f38fe918cdf5f24e8
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-26 14:07:11 +0100

    style[PurchaseCalculator]: add javadoc to constructor, and indent line

--- src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/transaction/PurchaseCalculator.java
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/transaction/PurchaseCalculator.java
@@ -1,9 +1,13 @@
 package edu.ntnu.idi.idatt2003.group18v26.model.transaction;
+
 import edu.ntnu.idi.idatt2003.group18v26.model.property.Share;
 import java.math.BigDecimal;
+
 /**
- * The PurchaseCalculator class calculates the gross amount, commission, tax, and total for a purchase transaction.
- * It implements the TransactionCalculator interface and uses the purchase price and quantity from a Share object to
+ * The PurchaseCalculator class calculates the gross amount, commission, tax, and total for
+ * a purchase transaction.
+ * It implements the TransactionCalculator interface and uses the purchase price and
+ * quantity from a Share object to
  * perform the calculations.
  * 
  * <p>The gross amount is calculated as:
@@ -20,9 +24,14 @@ public class PurchaseCalculator implements TransactionCalculator{
   private BigDecimal purchasePrice;
   private BigDecimal quantity;
 
+  /**
+   * Sets the purchasePrice and quantity based on information in the Share.
+   * 
+   * @param share The share to calculate the purchase of. Must be non-null.
+   */
   public PurchaseCalculator(Share share) {
     if (share == null) {
-    throw new IllegalArgumentException("Share cannot be null");
+      throw new IllegalArgumentException("Share cannot be null");
     }
     this.purchasePrice = share.purchasePrice();
     this.quantity = share.quantity();
