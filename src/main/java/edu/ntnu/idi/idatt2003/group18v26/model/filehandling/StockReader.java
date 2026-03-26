6eab6ab643574811c3fa78e61d9e416c25a14e85
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-26 13:26:21 +0100

    style[StockReader]: alphabetically sort imports, and cut off lines

--- src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/filehandling/StockReader.java
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/filehandling/StockReader.java
@@ -1,9 +1,9 @@
 package edu.ntnu.idi.idatt2003.group18v26.model.filehandling;
 
+import edu.ntnu.idi.idatt2003.group18v26.model.property.Stock;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
-import edu.ntnu.idi.idatt2003.group18v26.model.property.Stock;
 
 /**
  * Interface for reading stock data from a file. Implementations of this interface should provide
@@ -14,9 +14,10 @@ import edu.ntnu.idi.idatt2003.group18v26.model.property.Stock;
 public interface StockReader {
   /**
    * Reads a list of Stock objects from a file at the specified path.
-   * @param path the file path from which the stock data should be read
-   * @return a list of Stock objects read from the file
-   * @throws IOException if an I/O error occurs while reading from the file
+   * 
+   * @param path the file path from which the stock data should be read.
+   * @return a list of Stock objects read from the file.
+   * @throws IOException if an I/O error occurs while reading from the file.
    */
   List<Stock> readStocks(Path path) throws IOException;
 }
