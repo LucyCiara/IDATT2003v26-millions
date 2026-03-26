2d887ec5ead86a8118fc5906ac3ab771c2347401
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2026-03-26 14:04:25 +0100

    style[purchase]: add javadocs and break up line

--- src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/transaction/Purchase.java
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/transaction/Purchase.java
@@ -3,17 +3,34 @@ package edu.ntnu.idi.idatt2003.group18v26.model.transaction;
 import edu.ntnu.idi.idatt2003.group18v26.model.Player;
 import edu.ntnu.idi.idatt2003.group18v26.model.property.Share;
 
+/**
+ * A class to perform a purchase of a share using a player.
+ */
 public class Purchase extends Transaction {
+  /**
+   * The constructor sets up a calculator, and sets some information about the transaction and
+   * Share.
+   * 
+   * @param share The Share to buy.
+   * @param week The week of the Transaction.
+   */
   public Purchase(Share share, int week) {
     super(share, week, new PurchaseCalculator(share));
   }
 
+  /**
+   * Commits a purchase on a player, withdrawing the money from the Player, and adding the Share to
+   * the Player's Portfolio and TransactionArchive.
+   * 
+   * @param player The player the purchase is performed on. Must be non-null.
+   */
   @Override
   public void commit(Player player) {
     if (player == null) {
       throw new IllegalArgumentException("player can't be null");
     }
-    if (this.getCalculator().calculateTotal().compareTo(player.getMoney()) <= 0 && !this.committed) {
+    if (this.getCalculator().calculateTotal().compareTo(player.getMoney()) <= 0
+        && !this.committed) {
       player.withdrawMoney(this.getCalculator().calculateTotal());
       player.getPortfolio().addShare(this.getShare());
       player.getTransactionArchive().add(this);
