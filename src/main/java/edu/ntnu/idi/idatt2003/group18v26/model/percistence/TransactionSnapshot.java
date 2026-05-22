b56c54d81ca78a42436b7dd74f01bd39837fdc57
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-05-22 20:17:35 +0200

    feat[model/percistence]: Add Snapshot classes to assist with JSON saving

---
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/model/percistence/TransactionSnapshot.java
@@ -0,0 +1,30 @@
+package edu.ntnu.idi.idatt2003.group18v26.model.percistence;
+
+import java.math.BigDecimal;
+
+/**
+ * Data Transfer Object for a single transaction (buy or sell).
+ * Used when serializing transaction archive to JSON.
+ */
+public class TransactionSnapshot {
+  public String type;  // "PURCHASE" or "SALE"
+  public String symbol;
+  public int quantity;
+  public BigDecimal totalPrice;
+
+  /**
+   * Default constructor for Gson deserialization.
+   */
+  public TransactionSnapshot() {
+  }
+
+  /**
+   * Constructor for creating transaction snapshots.
+   */
+  public TransactionSnapshot(String type, String symbol, int quantity, BigDecimal totalPrice) {
+    this.type = type;
+    this.symbol = symbol;
+    this.quantity = quantity;
+    this.totalPrice = totalPrice;
+  }
+}
