7fe5f9835a684f2eabe9c6ed373096c8d76cf5b4
LucyCiara <lucycht@outlook.com>
2026-05-27 00:40:34 +0200

    fix[ButtonType]: fix button-text clipping from low button width by adding text-wrap

--- src/main/java/edu/ntnu/idi/idatt2003/group18v26/view/components/buttons/ButtonType.java
+++ src/main/java/edu/ntnu/idi/idatt2003/group18v26/view/components/buttons/ButtonType.java
@@ -3,12 +3,16 @@ package edu.ntnu.idi.idatt2003.group18v26.view.components.buttons;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.text.TextAlignment;
 
 public abstract class ButtonType extends Button {
   protected ButtonType() {
     setId("button1");
     getStyleClass().add("button");
+    setWrapText(true);
+    setTextAlignment(TextAlignment.CENTER);
   }
 
 }
